00ddc05e972cb63ff14ac1f8a8ff3754eeeddee5
Almutawaj <muntadher.contact@gmail.com>
2025-05-09 19:41:35 +0300

    Edit Logger

--- backend/src/lifespan.py
+++ backend/src/lifespan.py
@@ -5,7 +5,7 @@ import logging
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("--->") # Create a custom logger
 
 
 async def lifespan(app: FastAPI) -> AsyncGenerator:
