7efa06cf510f5c0a12ee2299a5be0edae3f6307a
Almutawaj <muntadher.contact@gmail.com>
2025-05-10 14:16:34 +0300

    Modify Get User

--- backend/src/utils/auth.py
+++ backend/src/utils/auth.py
@@ -1,25 +1,25 @@
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Request, HTTPException, status
 from jose import jwt, JWTError
 from config import settings
-from models.user import User  # your Beanie user model
+from models.user import User
+from starlette import status
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
+async def get_current_user(request: Request) -> User:
+    """Extracts and verifies user from JWT in cookies"""
+    if not (token := request.cookies.get("access_token")):
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, "Missing access token")
 
-async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
     try:
         payload = jwt.decode(
             token, settings.jwt_secret_key, algorithms=[settings.jwt_algorithm]
         )
-        email: str = payload.get("sub")
-        if email is None:
-            raise HTTPException(status_code=401, detail="Invalid token")
+        if not (email := payload.get("sub")):
+            raise HTTPException(status.HTTP_401_UNAUTHORIZED, "Invalid token payload")
+
+        if user := await User.find_one(User.email == email):
+            return user
+        raise HTTPException(status.HTTP_404_NOT_FOUND, "User not found")
+
     except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid token or expired")
-
-    user = await User.find_one(User.email == email)
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-
-    return user
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, "Invalid or expired token")
